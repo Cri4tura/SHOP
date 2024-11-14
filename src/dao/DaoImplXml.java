74b7bf57b44812750b6f55155f78dc33c44a2e76
marti <marti@DESKTOP-H2CRBBR>
2024-11-14 22:01:49 +0100

    Fix

---
+++ src/dao/DaoImplXml.java
@@ -0,0 +1,73 @@
+package dao;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+
+import dao.xml.DomWriter;
+import model.Employee;
+import model.Product;
+import dao.xml.SaxReader;
+
+public class DaoImplXml implements Dao {
+
+	public ArrayList<Product> inventory;
+
+	@Override
+	public void connect() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void disconnect() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ArrayList<Product> getInventory() {
+		inventory = new ArrayList<Product>();
+
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		SAXParser parser;
+
+		try {
+			parser = factory.newSAXParser();
+			File f = new File(System.getProperty("user.dir") + File.separator + "files/inputInventory.xml");
+			SaxReader saxReader = new SaxReader();
+			parser.parse(f, saxReader);
+			inventory = saxReader.getProducts();
+			return inventory;
+
+		} catch (ParserConfigurationException | SAXException e) {
+			System.out.println("ERROR parseando el inventario");
+			return null;
+		} catch (IOException e) {
+			System.out.println("ERROR fichero no encontrado");
+			return null;
+		}
+	}
+
+	@Override
+	public boolean writeInventory(ArrayList<Product> products) {
+		boolean generated = false;
+
+		DomWriter domWriter = new DomWriter();
+		generated = domWriter.generateDocument(inventory);
+		return generated;
+	}
+
+	@Override
+	public Employee getEmployee(int employeeId, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/dao/xml/DomWriter.java
@@ -0,0 +1,107 @@
+package dao.xml;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import model.Product;
+
+public class DomWriter {
+
+	private Document document;
+
+	public DomWriter() {
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newDefaultInstance();
+			DocumentBuilder builder;
+			builder = factory.newDocumentBuilder();
+			document = builder.newDocument();
+		} catch (ParserConfigurationException e) {
+			System.out.println("ERROR generando fichero XML");
+		}
+	}
+
+	public boolean generateDocument(ArrayList<Product> inventory) {
+
+		boolean generated = false;
+
+		Element products = document.createElement("products");
+		products.setAttribute("total", "" + inventory.size());
+		document.appendChild(products);
+
+		for (int i = 0; i < inventory.size(); i++) {
+			Element product = document.createElement("product");
+			product.setAttribute("id", "" + (i + 1));
+			products.appendChild(product);
+
+			Element name = document.createElement("name");
+			name.setTextContent(inventory.get(i).getName());
+			product.appendChild(name);
+
+			Element price = document.createElement("price");
+			price.setAttribute("currency", "€");
+			price.setTextContent("" + inventory.get(i).getWholesalerPrice());
+			product.appendChild(price);
+
+			Element stock = document.createElement("stock");
+			stock.setTextContent("" + inventory.get(i).getStock());
+			product.appendChild(stock);
+
+		}
+
+		generated = generateXml();
+		return generated;
+	}
+
+	private boolean generateXml() {
+		boolean generated = false;
+		try {
+			TransformerFactory factory = TransformerFactory.newInstance();
+			Transformer transformer = factory.newTransformer();
+
+			// Give format to the generated document
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");  
+			Source source = new DOMSource(document);
+
+			// Date
+			Date date = new Date();
+			String formatDate = new SimpleDateFormat("yyyy-MM-dd").format(date);
+
+			File f = new File("files" + File.separator + "inventory_" + formatDate + ".xml");
+			FileWriter fw = new FileWriter(f);
+			PrintWriter pw = new PrintWriter(fw);
+			Result result = new StreamResult(pw);
+
+			transformer.transform(source, result);
+			generated = true;
+		} catch (IOException e) {
+			System.out.println("ERROR creando el documento");
+			generated = false;
+		} catch (TransformerException e) {
+			System.out.println("ERROR dando formato al documento");
+			generated = false;
+		}
+		return generated;
+	}
+
+}
